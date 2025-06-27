e06ad2e36896dcc72a568995a1e5d5db08e8141f
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-06-27 09:08:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tovar[] tovary =   
+            {
+                new Book { Name = "Роман", Price = 10, Kvo = 3 },
+                new Pen { Name = "Ручка", Price = 3, Kvo = 5 },
+                new Candy { Name = "Конфеты", Price = 8, Ves = 6 }
+            };
+
+            foreach (var t in tovary)
+                t.Calc();
+        }
+    }
+}
---
+++ Tovar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Tovar
+    {
+        public string Name;
+        public int Price;
+        public virtual void Calc() { }
+    }
+}
